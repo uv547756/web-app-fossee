52dea96af1c8404ee5eddea820ec54a5f7561b7c
Utkarsh <59288856+uv547756@users.noreply.github.com>
2025-11-24 01:38:36 +0530

    Update SECRET_KEY in settings.py
    Replaced SECRET_KEY environment variable with a hardcoded value, for simplicity.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,10 +11,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY_DJANGO')
+SECRET_KEY ='django-insecure-blxi=!ozzm9yal7bf$5rzn6&wl^@&j)w&ai)fnn6g-)vl4*b&t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
